54d3029aad69bff894b48a12f319126eca6ac13b
Ahmet Yasin Bağçıvan <TheLastNever>
2024-05-22 13:39:27 +0300

    first commit

---
+++ HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        private ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetHelloWorld")]
+        public object Get()
+        {
+            return Ok("Hello World");
+        }
+
+    }
+}
